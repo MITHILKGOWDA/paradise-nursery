5c580084670742f411d0055b71a3a8a556149c73
MITHILKGOWDA <mkgmithil@gmail.com>
2026-01-11 13:35:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 
 function App() {
+  const handleGetStarted = () => {
+    console.log("Get Started clicked");
+  };
+
   return (
-    <div>
-      <h1>Paradise Nursery</h1>
-      <button>Get Started</button>
+    <div className="landing">
+      <h1>Welcome to Paradise Nursery</h1>
+      <button onClick={handleGetStarted}>Get Started</button>
     </div>
   );
 }
