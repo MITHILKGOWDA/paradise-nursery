7a59f5cae307dc1eb046c5af6abac6abd3c40db5
MITHILKGOWDA <mkgmithil@gmail.com>
2026-01-11 13:36:08 +0530

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,19 +1,63 @@
+import { useDispatch } from "react-redux";
+import { addItem } from "../redux/CartSlice";
+
 const ProductList = () => {
+  const dispatch = useDispatch();
+
+  const plantsArray = [
+    {
+      category: "Indoor Plants",
+      plants: [
+        { id: 1, name: "Snake Plant", price: 15 },
+        { id: 2, name: "Peace Lily", price: 18 },
+        { id: 3, name: "Rubber Plant", price: 20 },
+        { id: 4, name: "ZZ Plant", price: 22 },
+        { id: 5, name: "Pothos", price: 12 },
+        { id: 6, name: "Philodendron", price: 16 }
+      ]
+    },
+    {
+      category: "Succulents",
+      plants: [
+        { id: 7, name: "Aloe Vera", price: 10 },
+        { id: 8, name: "Jade Plant", price: 14 },
+        { id: 9, name: "Echeveria", price: 11 },
+        { id: 10, name: "Haworthia", price: 13 },
+        { id: 11, name: "Cactus", price: 9 },
+        { id: 12, name: "Sedum", price: 8 }
+      ]
+    },
+    {
+      category: "Air Purifying Plants",
+      plants: [
+        { id: 13, name: "Areca Palm", price: 25 },
+        { id: 14, name: "Spider Plant", price: 14 },
+        { id: 15, name: "Boston Fern", price: 17 },
+        { id: 16, name: "Dracaena", price: 19 },
+        { id: 17, name: "Bamboo Palm", price: 21 },
+        { id: 18, name: "Chinese Evergreen", price: 23 }
+      ]
+    }
+  ];
+
   return (
     <div>
-      <h2>Plants</h2>
+      <h2>Our Plants</h2>
 
-      <h3>Indoor Plants</h3>
-      <p>Snake Plant - $15</p>
-      <p>Peace Lily - $18</p>
+      {plantsArray.map((category) => (
+        <div key={category.category}>
+          <h3>{category.category}</h3>
 
-      <h3>Succulents</h3>
-      <p>Aloe Vera - $12</p>
-      <p>Jade Plant - $14</p>
-
-      <h3>Air Purifying</h3>
-      <p>Areca Palm - $20</p>
-      <p>Spider Plant - $13</p>
+          {category.plants.map((plant) => (
+            <div key={plant.id}>
+              <p>{plant.name} - ${plant.price}</p>
+              <button onClick={() => dispatch(addItem(plant))}>
+                Add to Cart
+              </button>
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
