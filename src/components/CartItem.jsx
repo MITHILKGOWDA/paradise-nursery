b8a663851afc4bcb290f79ec8f47d17f5eb169a4
MITHILKGOWDA <mkgmithil@gmail.com>
2026-01-11 13:36:47 +0530

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,1 +1,63 @@
+import { useSelector, useDispatch } from "react-redux";
+import { removeItem, updateQuantity } from "../redux/CartSlice";
+
+const CartItem = () => {
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const totalCost = cart.reduce(
+    (total, item) => total + item.price * (item.quantity || 1),
+    0
+  );
+
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      <p>Total Items: {cart.length}</p>
+      <p>Total Cost: ${totalCost}</p>
+
+      {cart.map((item) => (
+        <div key={item.id}>
+          <p>{item.name}</p>
+          <p>Unit Price: ${item.price}</p>
+
+          <button
+            onClick={() =>
+              dispatch(
+                updateQuantity({
+                  id: item.id,
+                  quantity: (item.quantity || 1) + 1
+                })
+              )
+            }
+          >
+            +
+          </button>
+
+          <button
+            onClick={() =>
+              dispatch(
+                updateQuantity({
+                  id: item.id,
+                  quantity: Math.max((item.quantity || 1) - 1, 1)
+                })
+              )
+            }
+          >
+            -
+          </button>
+
+          <button onClick={() => dispatch(removeItem(item.id))}>
+            Delete
+          </button>
+        </div>
+      ))}
+
+      <button>Continue Shopping</button>
+      <button>Checkout (Coming Soon)</button>
+    </div>
+  );
+};
+
+export default CartItem;
 
