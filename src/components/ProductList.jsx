b240188507df7bb0ec93741939c5b6dbbe0778a0
MITHILKGOWDA <mkgmithil@gmail.com>
2026-01-11 13:27:37 +0530

    Create ProductList.jsx

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,21 @@
+const ProductList = () => {
+  return (
+    <div>
+      <h2>Plants</h2>
+
+      <h3>Indoor Plants</h3>
+      <p>Snake Plant - $15</p>
+      <p>Peace Lily - $18</p>
+
+      <h3>Succulents</h3>
+      <p>Aloe Vera - $12</p>
+      <p>Jade Plant - $14</p>
+
+      <h3>Air Purifying</h3>
+      <p>Areca Palm - $20</p>
+      <p>Spider Plant - $13</p>
+    </div>
+  );
+};
+
+export default ProductList;
