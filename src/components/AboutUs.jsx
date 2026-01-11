0ba8511f6ad28094cb5894e427f9a60aae6a6101
MITHILKGOWDA <mkgmithil@gmail.com>
2026-01-11 13:27:09 +0530

    Create AboutUs.jsx

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,13 @@
+const AboutUs = () => {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery provides high-quality indoor plants
+        to bring nature into your home.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
